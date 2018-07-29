364369d49f65eddb79f02838e252d01244c53165
Ron Fridman <fridmanron@gmail.com>
2018-07-29 16:42:16 +0300

    bot update - pai-os

--- PAI.js
+++ PAI.js
@@ -12,23 +12,20 @@ const { Config } = require('@pai-tech/pai-net-sdk');
 const { PAIFileConnector, PAIHTTPConnector } = require('@pai-tech/pai-conntectors');
 const PAIBotManager = require('./src/pai-bot/src/pai-bot-manager');
 const readline = require('readline');
-
+const { PAI_OS } = require('@pai-tech/pai-os');
 
 let manager = new PAIBotManager();
 let fileConnector;
 let httpConnector;
-let pcmPAI_NET;
-
 
 
 async function start()
 {
     try{
-        initPAINET();
-        pcmPAI_NET = new PAINETModule();
         
         
-        await PAICode.loadModule('pai-net',pcmPAI_NET);
+        await PAICode.loadModule('pai-os',new PAI_OS());
+        await PAICode.loadModule('pai-net',new PAINETModule());
         
         
         let activeBot = await manager.loadBots();
@@ -47,6 +44,30 @@ async function start()
         
         // httpConnector = new PAIHTTPConnector( { port:3000 } );
         // httpConntector.start();
+        
+        
+        
+        setInterval(async () => {
+            let result = await PAICode.executeString('pai-net get-messages');
+            result = result[0];
+            
+            if(result.response.success)
+            {
+                let responses = result.response.data;
+                let display = [];
+                for (let i = 0; i < responses.length; i++) {
+                    let commandsInMsg =  responses[i];
+                    for (let j = 0; j < commandsInMsg.length; j++) {
+                        display.push(commandsInMsg[j].response);
+                    }
+                }
+    
+                if(display.length > 0)
+                    console.log(display);
+            }
+            
+        },1000);
+
     
         PAICode.start();
     } catch (e) {
