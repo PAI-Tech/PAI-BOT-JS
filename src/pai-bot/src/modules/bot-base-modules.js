fb99a133c1b389cbaa3394394c5a11f84b8c7739
Ron Fridman <fridmanron@gmail.com>
2018-11-20 17:55:52 +0200

    bot files Storage data source

--- src/pai-bot/src/modules/bot-base-modules.js
+++ src/pai-bot/src/modules/bot-base-modules.js
@@ -1,6 +1,7 @@
 const { PAINETModule } = require('@pai-tech/pai-net');
 const { PAI_OS } = require('@pai-tech/pai-os');
 const PAIModuleConfigStorageFiles = require('../../../pai-module-config-storage-files/pai-module-config-storage-files');
+const PAIFilesStorageDataSource = require('../../../pai-module-config-storage-files/pai-module-data-source-files');
 const PAIBotModule = require('../../pcm-pai-bot');
 const path = require('path');
 const PAISchedulerModule = require('@pai-tech/pai-scheduler').Module;
@@ -20,6 +21,19 @@ const modules = [
 
 let modulesLoaded = false;
 
+
+function setModuleConfigStorage(botSettingsFolder,module) {
+    module.config.storage = new PAIModuleConfigStorageFiles({
+        filePath: botSettingsFolder + module.setModuleName() + '.json'
+    });
+}
+
+function setModuleDataSource(botDataFolder,module) {
+    module.data.dataSource = new PAIFilesStorageDataSource({
+        filePath: botDataFolder + module.setModuleName()
+    });
+}
+
 /**
  * Load configuration for every module
  * @return {Promise<void>}
@@ -33,19 +47,15 @@ async function loadModulesConfig()
         if(!paiOSFolder)
             throw new Error('$PAI is not defined in server');
         
-        let botSettingsFolder = `${paiOSFolder}${path.sep}Bot${path.sep}settings${path.sep}`;
+        let botBaseFolder = `${paiOSFolder}${path.sep}Bot${path.sep}`;
+        let botSettingsFolder = `${botBaseFolder}settings${path.sep}`;
+        let botDataFolder = `${botBaseFolder}data${path.sep}`;
         
-        paiNET.config.storage = new PAIModuleConfigStorageFiles({
-            filePath: botSettingsFolder + paiNET.setModuleName() + '.json'
-        });
-        
-        paiBOT.config.storage = new PAIModuleConfigStorageFiles({
-            filePath: botSettingsFolder + paiBOT.setModuleName() + '.json'
-        });
     
-        paiScheduler.config.storage = new PAIModuleConfigStorageFiles({
-            filePath: botSettingsFolder + paiScheduler.setModuleName() + '.json'
-        });
+        for (let i = 0; i < modules.length; i++) {
+            setModuleConfigStorage(botSettingsFolder, modules[i]);
+            setModuleDataSource(botDataFolder,modules[i]);
+        }
         
         modulesLoaded = true;
     }
---
+++ src/pai-module-config-storage-files/pai-module-data-source-files.js
@@ -0,0 +1,179 @@
+const fs = require('fs');
+const path = require('path');
+const shell = require('shelljs');
+const {PAIEntity, PAIEntityList, PAIUtils, PAIDataSource, PAILogger} = require('@pai-tech/pai-code');
+
+/**
+ *
+ * @param entityData
+ * @param options
+ * @return {[PAIEntity]}
+ */
+function applyOptionsOnEntityData(entityData = [], options = {}) {
+    let { offset = 0, filters } = options;
+    
+    // TODO: apply options on entity data
+    
+    let newEntityData = entityData.filter(entity => {
+        for (let field in filters) {
+            let value = filters[field];
+            
+            if (entity[field] !== value)
+                return false;
+        }
+        
+        return true;
+    });
+    
+    return newEntityData;
+}
+
+
+
+
+/**
+ *
+ * @param {String} filePath
+ * @param {String} datashell
+ */
+function saveToFile(filePath, data){
+    return new Promise( (resolve,reject) => {
+        
+        fs.writeFile(filePath, data, 'utf8', function(err,data){
+            if(err)
+                return reject(err);
+            return resolve(data);
+        });
+        
+    });
+}
+
+/**
+ * @param {String} filePath
+ */
+function getFromFile(filePath){
+    return new Promise((resolve,reject) => {
+        
+        fs.readFile(filePath, 'utf-8' , function(err,data){
+            if(err)
+                return reject(err);
+            return resolve(data);
+        });
+        
+    })
+}
+
+
+/**
+ * @param {String} filePath
+ */
+function getAllFilesInDirectory(dirname){
+    return new Promise((resolve,reject) => {
+    
+        fs.readdir(dirname, function(err, data) {
+            if(err)
+                return reject(err);
+            return resolve(data);
+        });
+    })
+}
+
+
+/**
+ * @param {String} dirname
+ */
+async function readFilesInDirectory(dirname) {
+    
+    let filesData = [];
+    
+    
+    let allFiles = await getAllFilesInDirectory(dirname);
+    if(!allFiles)
+        return;
+        
+    for (let i = 0; i < allFiles.length; i++) {
+        let fileName = allFiles[i];
+        let fileData = await getFromFile(dirname + path.sep + fileName);
+        
+        filesData.push(fileData);
+    }
+    
+    return filesData;
+}
+
+
+
+
+
+class PAIFilesStorageDataSource extends PAIDataSource {
+    
+    
+    constructor(config) {
+        super();
+    
+    
+        let defaults = {
+            filePath: '/var/PAI/Bot/settings/module.json'
+        };
+    
+        this.config = Object.assign(defaults,config || {});
+        
+        
+        // create folder if not exists
+        if(!fs.existsSync(this.config.filePath))
+            shell.mkdir('-p',this.config.filePath);
+    }
+    
+    
+    async save(entity) {
+        let currentTime = new Date().getTime();
+        entity._id = PAIUtils.pai_guid();
+        entity.createdAt = currentTime;
+        entity.updatedAt = currentTime;
+
+        let entityFolder = this._getPathForEntity(entity);
+        await saveToFile(
+            entityFolder + path.sep + entity._id + ".json",
+            JSON.stringify(entity)
+        );
+        
+        
+        return entity;
+    }
+    
+    /**
+     *
+     * @param {PAIEntity} entity
+     * @param options
+     * @return {Promise<PAIEntityList>}
+     */
+    find(entity, options) {
+        return new Promise(async (resolve, reject) => {
+            let allRecordsInEntity = await readFilesInDirectory(this._getPathForEntity(entity));
+            
+            let filteredEntityData = applyOptionsOnEntityData(allRecordsInEntity, options);
+            
+            let result = new PAIEntityList();
+            result.count = filteredEntityData.length;
+            result.total = filteredEntityData.length;
+            result.offset = 0;
+            result.records = filteredEntityData;
+            
+            resolve(result);
+        });
+    }
+    
+    _getPathForEntity(entity) {
+    
+        let entityPath = this.config.filePath + path.sep + entity.setEntityName();
+
+        if(!fs.existsSync(entityPath))
+            fs.mkdirSync(entityPath);
+        
+        return entityPath;
+    }
+    
+}
+
+
+module.exports = PAIFilesStorageDataSource;
