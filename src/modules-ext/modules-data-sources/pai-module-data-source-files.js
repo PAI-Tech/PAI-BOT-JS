774909652e22c6ed87ebe09f9654fc60ddff3e9b
Ron Fridman <fridmanron@gmail.com>
2019-01-21 17:56:26 +0200

    file data source

--- src/modules-ext/modules-data-sources/pai-module-data-source-files.js
+++ src/modules-ext/modules-data-sources/pai-module-data-source-files.js
@@ -1,7 +1,7 @@
-const fs = require("fs");
-const path = require("path");
-const shell = require("shelljs");
-const {PAIEntity, PAIEntityList, PAIUtils, PAIBaseDataSource, PAILogger} = require("@pai-tech/pai-code");
+const fs = require('fs');
+const path = require('path');
+const shell = require('shelljs');
+const {PAIEntity, PAIEntityList, PAIUtils, PAIBaseDataSource, PAILogger} = require('@pai-tech/pai-code');
 
 /**
  *
@@ -10,22 +10,22 @@ const {PAIEntity, PAIEntityList, PAIUtils, PAIBaseDataSource, PAILogger} = requi
  * @return {[PAIEntity]}
  */
 function applyOptionsOnEntityData(entityData = [], options = {}) {
-    let { offset = 0, filters } = options;
-    
-    // TODO: apply options on entity data
-    
-    let newEntityData = entityData.filter(entity => {
-        for (let field in filters) {
-            let value = filters[field];
-            
-            if (entity[field] !== value)
-                return false;
-        }
-        
-        return true;
-    });
-    
-    return newEntityData;
+	let { offset = 0, filters } = options;
+	
+	// TODO: apply options on entity data
+	
+	let newEntityData = entityData.filter(entity => {
+		for (let field in filters) {
+			let value = filters[field];
+			
+			if (entity[field] !== value)
+				return false;
+		}
+		
+		return true;
+	});
+	
+	return newEntityData;
 }
 
 
@@ -37,15 +37,15 @@ function applyOptionsOnEntityData(entityData = [], options = {}) {
  * @param {String} data
  */
 function saveToFile(filePath, data){
-    return new Promise( (resolve,reject) => {
-        
-        fs.writeFile(filePath, data, "utf8", function(err,data){
-            if(err)
-                return reject(err);
-            return resolve(data);
-        });
-        
-    });
+	return new Promise( (resolve,reject) => {
+		
+		fs.writeFile(filePath, data, 'utf8', function(err,data){
+			if(err)
+				return reject(err);
+			return resolve(data);
+		});
+		
+	});
 }
 
 
@@ -55,30 +55,30 @@ function saveToFile(filePath, data){
  * @param {String} filePath
  */
 function deleteFile(filePath){
-    return new Promise( (resolve,reject) => {
-        
-        fs.unlink(filePath, function(err,data){
-            if(err)
-                return reject(err);
-            return resolve(data);
-        });
-        
-    });
+	return new Promise( (resolve,reject) => {
+		
+		fs.unlink(filePath, function(err,data){
+			if(err)
+				return reject(err);
+			return resolve(data);
+		});
+		
+	});
 }
 
 /**
  * @param {String} filePath
  */
 function getFromFile(filePath){
-    return new Promise((resolve,reject) => {
-        
-        fs.readFile(filePath, "utf-8" , function(err,data){
-            if(err)
-                return reject(err);
-            return resolve(data);
-        });
-        
-    });
+	return new Promise((resolve,reject) => {
+		
+		fs.readFile(filePath, 'utf-8' , function(err,data){
+			if(err)
+				return reject(err);
+			return resolve(data);
+		});
+		
+	})
 }
 
 
@@ -86,16 +86,16 @@ function getFromFile(filePath){
  * @param {String} dirPath
  */
 function getAllFilesInDirectory(dirPath){
-    return new Promise((resolve,reject) => {
-    
-        fs.readdir(dirPath, function(err, data) {
-            if(err)
-                return reject(err);
-            return resolve(data.filter(file => {
-                return file.indexOf(".json") >= 0;
-            }));
-        });
-    });
+	return new Promise((resolve,reject) => {
+		
+		fs.readdir(dirPath, function(err, data) {
+			if(err)
+				return reject(err);
+			return resolve(data.filter(file => {
+				return file.indexOf(".json") >= 0;
+			}));
+		});
+	})
 }
 
 
@@ -103,33 +103,34 @@ function getAllFilesInDirectory(dirPath){
  * @param {String} dirPath
  */
 async function readFilesInDirectory(dirPath) {
-    
-    let filesData = [];
-    
-    
-    let allFiles = await getAllFilesInDirectory(dirPath);
-    if(!allFiles)
-        return;
-        
-    for (let i = 0; i < allFiles.length; i++) {
-        let fileName = allFiles[i];
-        let fileData = await getFromFile(dirPath + path.sep + fileName);
-        
-        if(fileData)
-            filesData.push(JSON.parse(fileData));
-        
-        
-    }
-    
-    return filesData;
+	
+	let filesData = [];
+	
+	
+	let allFiles = await getAllFilesInDirectory(dirPath);
+	if(!allFiles)
+		return;
+	
+	for (let i = 0; i < allFiles.length; i++) {
+		let fileName = allFiles[i];
+		let fileData = await getFromFile(dirPath + path.sep + fileName);
+		
+		if(fileData)
+			filesData.push(JSON.parse(fileData));
+		
+		
+	}
+	
+	return filesData;
 }
 
 
 function convertToEntities(entity, data) {
-    
-    let ent = Object.create(entity);
-    ent = Object.assign(ent,data);
-    return ent;
+	
+	let ent = {};
+	Object.assign(ent, entity);
+	ent = Object.assign(ent,data);
+	return ent;
 }
 
 
@@ -137,126 +138,144 @@ function convertToEntities(entity, data) {
 
 
 class PAIFilesStorageDataSource extends PAIBaseDataSource {
-    
-    
-    constructor(config) {
-        super();
-    
-    
-        let defaults = {
-            filePath: "/var/PAI/Bot/settings/module.json"
-        };
-    
-        this.config = Object.assign(defaults,config || {});
-        
-        
-        // create folder if not exists
-        if(!fs.existsSync(this.config.filePath))
-            shell.mkdir("-p",this.config.filePath);
-    }
-    
-    
-    async save(entity) {
-        let currentTime = new Date().getTime();
-        entity._id = PAIUtils.pai_guid();
-        entity.createdAt = currentTime;
-        entity.updatedAt = currentTime;
-
-        let entityFolder = this._getPathForEntity(entity);
-        await saveToFile(
-            entityFolder + path.sep + entity._id + ".json",
-            JSON.stringify(entity)
-        );
-        
-        
-        return entity;
-    }
-    
-    /**
-     *
-     * @param {PAIEntity} entity
-     * @param options
-     * @return {Promise<PAIEntityList>}
-     */
-    find(entity, options) {
-        return new Promise(async (resolve, reject) => {
-            let dirPath = this._getPathForEntity(entity);
-    
-    
-            let allFilesInDir = await readFilesInDirectory(dirPath).catch(err => {
-                throw err;
-            });
-            
-            let allRecordsInEntity = allFilesInDir.map(data => {
-               return convertToEntities(entity,data);
-            });
-            
-            let filteredEntityData = applyOptionsOnEntityData(allRecordsInEntity, options);
-            
-            let result = new PAIEntityList();
-            result.count = filteredEntityData.length;
-            result.total = filteredEntityData.length;
-            result.offset = 0;
-            result.records = filteredEntityData;
-            
-            resolve(result);
-        });
-    }
-    
-    
-    
-    
-    update(entity) {
-        return new Promise(async (resolve, reject) => {
-            
-            if(!entity._id) {
-                return reject(new Error("Missing entity._id, required for update"));
-            }
-            
-            let currentTime = new Date().getTime();
-            entity.updatedAt = currentTime;
-    
-            let entityFolder = this._getPathForEntity(entity);
-            await saveToFile(
-                entityFolder + path.sep + entity._id + ".json",
-                JSON.stringify(entity)
-            );
-            
-    
-            resolve(entity);
-        });
-    }
-    
-    
-    
-    delete(entity) {
-        return new Promise(async (resolve, reject) => {
-            
-            if(!entity._id) {
-                return reject(new Error("Missing entity._id, required for delete"));
-            }
-    
-    
-            let entityFolder = this._getPathForEntity(entity);
-            await deleteFile(
-                entityFolder + path.sep + entity._id + ".json"
-            );
-            
-            resolve(entity);
-        });
-    }
-    
-    
-    _getPathForEntity(entity) {
-    
-        let entityPath = this.config.filePath + path.sep + entity.setEntityName();
-
-        if(!fs.existsSync(entityPath))
-            fs.mkdirSync(entityPath);
-        
-        return entityPath;
-    }
-    
+	
+	
+	constructor(config) {
+		super();
+		
+		
+		let defaults = {
+			filePath: '/var/PAI/Bot/settings/module.json'
+		};
+		
+		this.config = Object.assign(defaults,config || {});
+		
+		
+		// create folder if not exists
+		if(!fs.existsSync(this.config.filePath))
+			shell.mkdir('-p',this.config.filePath);
+	}
+	
+	
+	async save(entity) {
+		let currentTime = new Date().getTime();
+		entity._id = PAIUtils.pai_guid();
+		entity.createdAt = currentTime;
+		entity.updatedAt = currentTime;
+		
+		let entityFolder = this._getPathForEntity(entity);
+		
+		
+		let entityObjToSave = {};
+		
+		const fields = entity.__entity_schema.fields;
+		const fieldKeys = Object.keys(fields);
+		
+		for (let i = 0; i < fieldKeys.length; i++) {
+			const fieldName = fieldKeys[i];
+			const fieldValue = fields[fieldName].value;
+			
+			if(fieldValue !== undefined && fieldValue !== null)
+			{
+				entityObjToSave[fieldName] = fieldValue;
+			}
+		}
+		
+		
+		await saveToFile(
+			entityFolder + path.sep + entity._id + ".json",
+			JSON.stringify(entityObjToSave)
+		);
+		
+		
+		return entity;
+	}
+	
+	/**
+	 *
+	 * @param {PAIEntity} entity
+	 * @param options
+	 * @return {Promise<PAIEntityList>}
+	 */
+	find(entity, options) {
+		return new Promise(async (resolve, reject) => {
+			let dirPath = this._getPathForEntity(entity);
+			
+			
+			let allFilesInDir = await readFilesInDirectory(dirPath).catch(err => {
+				throw err;
+			});
+			
+			let allRecordsInEntity = allFilesInDir.map(data => {
+				return convertToEntities(entity,data);
+			});
+			
+			let filteredEntityData = applyOptionsOnEntityData(allRecordsInEntity, options);
+			
+			let result = new PAIEntityList();
+			result.count = filteredEntityData.length;
+			result.total = filteredEntityData.length;
+			result.offset = 0;
+			result.records = filteredEntityData;
+			
+			resolve(result);
+		});
+	}
+	
+	
+	
+	
+	update(entity) {
+		return new Promise(async (resolve, reject) => {
+			
+			if(!entity._id) {
+				return reject(new Error('Missing entity._id, required for update'));
+			}
+			
+			let currentTime = new Date().getTime();
+			entity.updatedAt = currentTime;
+			
+			let entityFolder = this._getPathForEntity(entity);
+			await saveToFile(
+				entityFolder + path.sep + entity._id + ".json",
+				JSON.stringify(entity)
+			);
+			
+			
+			resolve(entity);
+		});
+	}
+	
+	
+	
+	delete(entity) {
+		return new Promise(async (resolve, reject) => {
+			
+			if(!entity._id) {
+				return reject(new Error('Missing entity._id, required for delete'));
+			}
+			
+			
+			let entityFolder = this._getPathForEntity(entity);
+			await deleteFile(
+				entityFolder + path.sep + entity._id + ".json"
+			);
+			
+			resolve(entity);
+		});
+	}
+	
+	
+	_getPathForEntity(entity) {
+		
+		let entityPath = this.config.filePath + path.sep + entity.setEntityName();
+		
+		if(!fs.existsSync(entityPath))
+			fs.mkdirSync(entityPath);
+		
+		return entityPath;
+	}
+	
 }
 
 
