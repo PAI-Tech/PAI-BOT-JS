23be65c6f6a058debf52f28c7b94a0a7b9a90685
Roie Finkelshtein <41997967+roiefink@users.noreply.github.com>
2021-01-31 20:28:48 +0200

    Update pai-code-run.js

--- pai-code-run.js
+++ pai-code-run.js
@@ -5,7 +5,6 @@ const PAIBotOSUtils = require("./src/pai-bot/src/utils/pai-bot-os-utils");
 
 const run = async () => {
     const queueFolder = await PAIBotOSUtils.getBotQueueFolder();
-    console.log(pai_code);
     fs.writeFileSync(`${queueFolder}in.pai`, pai_code, 'utf8');
     console.log('Done');
 };
